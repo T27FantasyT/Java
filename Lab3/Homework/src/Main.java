2a3038c5eae252bf6c7dda524f58590a80f589d6
T27 <nguyenvantien.k51ktqt.ftu@gmail.com>
2021-12-22 19:55:18 +0700

    a

--- Lab3/Homework/src/Main.java
+++ Lab3/Homework/src/Main.java
@@ -3,20 +3,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        simpleEquation();
+        // Chương trình đếm số kí tự
+        charCount();
 
-    }
-    public static void simpleEquation(){
+        //Chương trình giải phương trình bậc nhất
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập vào tham số a: ");
+        System.out.println("Nhập vào hệ số a: ");
         double a = sc.nextDouble();
         System.out.println("Nhập vào tham số b: ");
         double b = sc.nextDouble();
-
-        if (a!=0){
-            System.out.println("Nghiệm của phương trình là: "+-b/a);
+        if (a != 0) {
+            System.out.printf("Nghiệm của phương trình là x= " + simpleEquation(a, b));
         } else {
             System.out.println("Phương trình đúng với mọi x");
         }
+    }
+
+    public static void charCount() {
+        String str = "You only live once, but if you do it right, once is enough";
+        int i = 0;
+        int count = 1;
+
+        while (i < str.length()) {
+            if (str.charAt(i) == 'o') {
+                int j = i + 1;
+                System.out.println("Kí tự 'o' thứ " + count + " ở vị trí thứ " + j);
+                count++;
+            }
+            i++;
+        }
+    }
+    public static double simpleEquation(double a, double b) {
+        return  -b / a;
     }
 }
