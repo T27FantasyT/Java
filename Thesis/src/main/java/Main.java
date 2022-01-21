c629fd944cf44e6507631e72d95f57337ba5c568
T.27..Fantasy.T <nguyenvantien.k51ktqt.ftu@gmai.com>
2022-01-21 21:19:48 +0700

    s

--- Thesis/src/main/java/Main.java
+++ Thesis/src/main/java/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        GetEmployee employee = new GetEmployee();
+        ArrayList<Employee> employeeArrayList = employee.getAllEmployee();
+        for(Employee e : employeeArrayList){
+            System.out.println(e);
+        }
     }
 }
